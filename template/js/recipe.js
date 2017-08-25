2321ee00d599584af3d7b7a6f278cff92eb6d059
Lawal Lanre E <larrystonegroup@gmail.com>
2017-08-26 00:47:03 +0100

    Create Recipe detail view template * View Recipe details * Add Recipe to favorites * Upvote Recipe * Downvote Recipe * Write a review on Recipe * View reviews on Recipe

--- template/js/recipe.js
+++ template/js/recipe.js
@@ -8,3 +8,29 @@ $('.button-collapse').sideNav({
 $('.showInfo').on('click', () => {
   window.location.href = 'recipe_info.html';
 });
+
+
+$('#bookmark').on('click', (e) => {
+  if ($(e.target).html() === 'star_border') {
+    $(e.target).html('star');
+  } else {
+    $(e.target).html('star_border');
+  }
+});
+
+$('#like').on('click', () => {
+  const likes = +$('#likes').text() + 1;
+  $('#likes').text(likes);
+});
+
+$('#dislike').on('click', () => {
+  const likes = +$('#dislikes').text() + 1;
+  $('#dislikes').text(likes);
+});
+
+$(() => {
+  setTimeout(() => {
+    $('#preload_reviews').css('display', 'none');
+    $('#reviews').removeClass('hide');
+  }, 3000);
+});
