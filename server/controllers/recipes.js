ffb763d59aa9a504a49743918403f9b3a2f7d7c0
Lawal Lanre E <larrystonegroup@gmail.com>
2017-08-30 15:38:25 +0100

    Create getUserRecipes function in recipes Controller

--- server/controllers/recipes.js
+++ server/controllers/recipes.js
@@ -20,6 +20,55 @@ export const createRecipe = (req, res) => {
       userId: req.session.user.id
     })
     .then((createdRecipe) => {
+      res.status(201).send(createdRecipe);
+    })
+    .catch(() => res.status(401).send({ error: 'Error Creating Recipe' }));
+
+  return newUser;
+};
+
+/**
+ * @exports getUserRecipes
+ * @param  {obj} req request object
+ * @param  {obj} res result object
+ * @return {obj}  newUser object
+ */
+export const getUserRecipes = (req, res) => {
+  const recipes = recipe
+    .findAll()
+    .then((foundRecipes) => {
+      if (!foundRecipes) {
+        return res.status(404).send({
+          message: 'No User Stored Recipes found',
+        });
+      }
+
+      return res.status(201).send(foundRecipes);
+    })
+    .catch(() => res.status(401).send('Unable to get user recipes'));
+
+  return recipes;
+};
+
+
+/**
+ * @exports editRecipe
+ * @param  {obj} req request object
+ * @param  {obj} res result object
+ * @return {obj}  newUser object
+ */
+export const editRecipe = (req, res) => {
+  const name = req.body.name;
+  const ingredients = req.body.ingredients || [];
+  const directions = req.body.directions || '';
+  const newUser = recipe
+    .create({
+      name,
+      ingredients,
+      directions,
+      userId: req.session.user.id
+    })
+    .then((createdRecipe) => {
       // const createdRecipe = {
       //   recipeId: result.id,
       //   name: result.name,
@@ -33,8 +82,4 @@ export const createRecipe = (req, res) => {
     .catch(() => res.status(401).send({ error: 'Error Creating Recipe' }));
 
   return newUser;
-};
-
-export const dd = () => {
-
 };
