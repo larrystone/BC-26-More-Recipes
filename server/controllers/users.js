4badc565795d154206e3cc18b755c50a93572f4e
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-01 11:11:08 +0100

    Check email address and username already exist

--- server/controllers/users.js
+++ server/controllers/users.js
@@ -14,25 +14,48 @@ export const signUp = (req, res) => {
   const username = (req.body.username || '').replace(' ', '');
   const email = (req.body.email || '').replace(' ', '');
   const newUser = user
-    .create({
-      name,
-      username,
-      email,
-      password: auth.generateHash(req.body.password),
+    .findOne({
+      attributes: ['id'],
+      where: {
+        $or: [
+          { username: {
+            $iLike: username }
+          },
+          { email: {
+            $iLike: email }
+          }
+        ]
+      }
     })
-    .then((result) => {
-      const loggedInUser =
-        { userId: result.id, username: result.username, email: result.email };
+    .then((userFound) => {
+      if (userFound) {
+        return res.status(401).send({
+          error: 'Username or email already taken!',
+        });
+      }
 
-      req.session.user = result;
+      user
+        .create({
+          name,
+          username,
+          email,
+          password: auth.generateHash(req.body.password),
+        })
+        .then((result) => {
+          const createdUser = {
+            userId: result.id,
+            username: result.username,
+            email: result.email };
 
-      return res.status(201).send(loggedInUser);
-    })
-    .catch(() => res.status(401).send({ error: 'Error Creating user' }));
+          req.session.user = result;
 
-  return newUser;
+          return res.status(201).send(createdUser);
+        })
+        .catch(() => res.status(401).send({ error: 'Error Creating user' }));
+
+      return newUser;
+    });
 };
-
 
 /**
  * @exports signIn
