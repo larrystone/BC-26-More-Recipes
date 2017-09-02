bb0acd2ce020c5985d261ae15c91437c45aec062
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-02 23:40:34 +0100

    Assign /:userId/recipes/:recipeId endpoint for addToFavorite

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 
 import * as userController from '../../controllers/users';
 import * as recipeController from '../../controllers/recipes';
+import * as favoriteController from '../../controllers/favorites';
+
 import * as auth from '../../controllers/auth';
 
 const user = express.Router();
@@ -12,5 +14,8 @@ user.post('/signout', userController.signOut);
 
 user.get('/myRecipes', auth.default);
 user.get('/myRecipes', recipeController.getUserRecipes);
+
+user.post('/:userId/recipes/:recipeId', favoriteController.addToFavorite);
+
 
 export default user;
