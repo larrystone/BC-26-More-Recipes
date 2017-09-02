1f6975df92aca19ebf68ee72f2064e1af8628d9f
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-03 00:11:45 +0100

    Assign DELETE /:userId/recipes/:recipeId endpoint for removeFavorite

--- server/routes/users/index.js
+++ server/routes/users/index.js
@@ -16,6 +16,8 @@ user.get('/myRecipes', auth.default);
 user.get('/myRecipes', recipeController.getUserRecipes);
 
 user.post('/:userId/recipes/:recipeId', favoriteController.addToFavorite);
+user.delete('/:userId/recipes/:recipeId', favoriteController.removeFromFavorites);
 user.get('/:userId/recipes', favoriteController.getFavRecipes);
+
 
 export default user;
