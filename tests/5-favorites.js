96efbc5a2c45e0358f247ce5749b3b2953340a81
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-05 20:01:02 +0100

    Add recipe to favorites

---
+++ tests/5-favorites.js
@@ -0,0 +1,48 @@
+import chai, { expect } from 'chai';
+import chaiHttp from 'chai-http';
+
+import server from './../server/app';
+
+chai.use(chaiHttp);
+
+let token;
+let recipeId;
+let userId;
+
+describe('/POST Create User and Recipe', () => {
+  it('should create a user (to get save token)', (done) => {
+    chai.request(server)
+      .post('/api/v1/users/signup')
+      .set('Accept', 'application/json')
+      .send({
+        name: 'Create Favorite Tester',
+        username: 'CreateFavTester',
+        email: 'createfavtester@test.com',
+        password: 'testing'
+      })
+      .end((err, res) => {
+        token = res.body.token;
+        userId = res.body.userId;
+        expect(res.statusCode).to.equal(201);
+        done();
+      });
+  });
+
+  it('should create a recipe (to get recipeId)', (done) => {
+    chai.request(server)
+      .post('/api/v1/recipes')
+      .set('Accept', 'application/json')
+      .send({
+        token,
+        name: 'Test Recipe',
+        ingredients: 'Item1;;Item2;;Item3',
+        direction: 'direction and direction and directions'
+      })
+      .end((err, res) => {
+        recipeId = res.body.id;
+        expect(res.statusCode).to.equal(201);
+        expect(res.body.success).to.equal(true);
+        done();
+      });
+  });
+});
