546ee072a9c9ec6eb57b3c1833dd49cb9b179769
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-06 10:43:14 +0100

    Fix app crash

--- tests/10-destroy-tables.js
+++ tests/10-destroy-tables.js
@@ -1,9 +1,44 @@
 import models from './../server/models';
 
 const user = models.User;
+const recipe = models.Recipe;
+const favorite = models.Favorite;
+const upvote = models.Upvote;
+const downvote = models.Downvote;
+const review = models.Review;
 
 describe('Destroy tables after tests', () => {
   user.destroy({
+    where: { },
+    truncate: true,
+    cascade: true
+  });
+
+  recipe.destroy({
+    where: { },
+    truncate: true,
+    cascade: true
+  });
+
+  favorite.destroy({
+    where: { },
+    truncate: true,
+    cascade: true
+  });
+
+  upvote.destroy({
+    where: { },
+    truncate: true,
+    cascade: true
+  });
+
+  downvote.destroy({
+    where: { },
+    truncate: true,
+    cascade: true
+  });
+
+  review.destroy({
     where: { },
     truncate: true,
     cascade: true
