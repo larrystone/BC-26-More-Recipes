1a96e8510e0b1ddd1485ee32278a39ff291cf196
Lawal Lanre E <larrystonegroup@gmail.com>
2017-09-06 03:33:35 +0100

    Fix bug on travis CI

--- server/models/index.js
+++ server/models/index.js
@@ -4,7 +4,7 @@ import Sequelize from 'sequelize';
 import configs from '../config/config';
 
 const basename = path.basename(module.filename);
-const env = process.env.NODE_ENV || 'development';
+const env = process.env.NODE_ENV || 'test';
 const config = configs[env];
 
 const db = {};
