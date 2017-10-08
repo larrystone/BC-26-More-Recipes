897aa57a784b7497b423ae2e14743fc16a4e5bb2
Lawal Lanre E <larrystonegroup@gmail.com>
2017-10-08 20:52:03 +0100

    Add logic to disable form inputs when connecting to backend

--- client/src/components/SignUpSignIn.jsx
+++ client/src/components/SignUpSignIn.jsx
@@ -13,7 +13,8 @@ class SignInSignUp extends Component {
       password1: '',
       password2: '',
       status: 'login',
-      error: ''
+      error: '',
+      loading: false
     };
   }
 
@@ -35,62 +36,71 @@ class SignInSignUp extends Component {
     }
   }
 
-  renderSignUp = () => (
-    <Modal open={true}>
-      <Modal.Header>
-        Sign Up for an Account
-      </Modal.Header>
-      <Modal.Content>
-        <Form>
-          <Form.Input
-            label='Enter your name'
-            placeholder='Enter your name'
-            onChange={(event) => {
-              this.storeToState('name', event.target.value)
-            }} />
-          <Form.Input
-            label='Username'
-            placeholder='Username'
-            onChange={(event) => {
-              this.storeToState('username', event.target.value)
-            }} />
-          <Form.Input
-            label='Email Address'
-            placeholder='Email Address'
-            onChange={(event) => {
-              this.storeToState('email', event.target.value)
-            }} />
-          <Form.Input
-            label='Password'
-            placeholder='Password'
-            type='password'
-            onChange={(event) => {
-              this.storeToState('password1', event.target.value)
-            }} />
-          <Form.Input
-            label='Re-enter Password'
-            placeholder='Re-enter Password'
-            type='password'
-            onChange={(event) => {
-              this.storeToState('password2', event.target.value)
-            }} />
-          <h6 className="red-text"><em>{this.state.error}</em></h6>
-          <Form.Button positive
-            onClick={(event) => {
-              this.handleSignUp(event);
-            }}>
-            Continue
-          </Form.Button>
-        </Form>
-      </Modal.Content>
-      <Modal.Actions>
-        Already have an account?
-      <Button>
-          Sign In
-      </Button>
-      </Modal.Actions>
-    </Modal>
-  )
+  renderSignUp = () => {
+    const { loading } = this.state;
+    return (
+      <Modal open={true}>
+        <Modal.Header>
+          Sign Up for an Account
+          </Modal.Header>
+        <Modal.Content>
+          <Form>
+            <Form.Input
+              disabled={loading}
+              label='Enter your name'
+              placeholder='Enter your name'
+              onChange={(event) => {
+                this.storeToState('name', event.target.value)
+              }} />
+            <Form.Input
+              disabled={loading}
+              label='Username'
+              placeholder='Username'
+              onChange={(event) => {
+                this.storeToState('username', event.target.value)
+              }} />
+            <Form.Input
+              disabled={loading}
+              label='Email Address'
+              placeholder='Email Address'
+              onChange={(event) => {
+                this.storeToState('email', event.target.value)
+              }} />
+            <Form.Input
+              disabled={loading}
+              label='Password'
+              placeholder='Password'
+              type='password'
+              onChange={(event) => {
+                this.storeToState('password1', event.target.value)
+              }} />
+            <Form.Input
+              disabled={loading}
+              label='Re-enter Password'
+              placeholder='Re-enter Password'
+              type='password'
+              onChange={(event) => {
+                this.storeToState('password2', event.target.value)
+              }} />
+            <h6 className="red-text"><em>{this.state.error}</em></h6>
+            <Form.Button positive
+              disabled={loading}
+              onClick={(event) => {
+                this.handleSignUp(event);
+              }}>
+              Continue
+              </Form.Button>
+          </Form>
+        </Modal.Content>
+        <Modal.Actions>
+          Already have an account?
+          <Button disabled={loading}>
+            Sign In
+          </Button>
+        </Modal.Actions>
+      </Modal>
+    )
+  }
 
   render() {
     console.log(this.props.dialogType)
