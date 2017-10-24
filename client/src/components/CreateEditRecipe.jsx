53c3d3f188fb4cfcfe20b106117df6e7cabbb9e4
Lawal Lanre E <larrystonegroup@gmail.com>
2017-10-24 08:16:31 +0100

    Implement recipe image upload in Recipe creation frontend

--- client/src/components/CreateEditRecipe.jsx
+++ client/src/components/CreateEditRecipe.jsx
@@ -3,11 +3,13 @@ import { Modal, Form, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import { read_cookie } from 'sfcookies';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 
 import { setDialogType } from '../actions/dialog';
 import { setReloadRecipes } from '../actions/reload_recipe';
 
-const TOKEN = read_cookie('more-recipe-token');
+const TOKEN = read_cookie('more-recipe-token'),
+  FIRST_INDEX = 0;
 
 class CreateEditRecipe extends Component {
   constructor(props) {
@@ -16,10 +18,11 @@ class CreateEditRecipe extends Component {
       name: '',
       description: '',
       ingredients: '',
+      imageUrl: '',
       direction: '',
       error: '',
       loading: false
-    }
+    };
   }
 
   storeToState(key, value) {
@@ -28,7 +31,7 @@ class CreateEditRecipe extends Component {
         [key]: value,
         error: ''
       }
-    )
+    );
   }
 
   componentDidMount() {
@@ -36,14 +39,17 @@ class CreateEditRecipe extends Component {
     if (recipeId) {
       this.setState(
         { loading: true }
-      )
+      );
       axios({
         method: 'GET',
         url: `/api/v1/recipes/${recipeId}`,
         headers: { 'x-access-token': TOKEN }
       })
         .then((response) => {
-          const { name, description, ingredients, direction } = response.data.recipe;
+          const {
+            name, description,
+            ingredients, direction
+          } = response.data.recipe;
           this.setState(
             {
               name,
@@ -52,7 +58,7 @@ class CreateEditRecipe extends Component {
               direction,
               loading: false
             }
-          )
+          );
         })
         .catch((error) => {
           this.setState(
@@ -60,16 +66,16 @@ class CreateEditRecipe extends Component {
               error: error.response.data.message,
               loading: false
             }
-          )
+          );
         });
     }
   }
 
-  closeModal = () => {
+  closeModal() {
     this.props.setDialogType('');
   }
 
-  updateRecipe = () => {
+  updateRecipe() {
     const { recipeId } = this.props;
     const { name, description, ingredients, direction } = this.state;
     this.setState(
@@ -81,7 +87,7 @@ class CreateEditRecipe extends Component {
       headers: { 'x-access-token': TOKEN },
       data: {
         name, description, direction,
-        ingredients: ingredients.replace(/,/g, ';;'),
+        ingredients: ingredients.replace(/,/g, ';;')
       }
     })
       .then((response) => {
@@ -97,12 +103,21 @@ class CreateEditRecipe extends Component {
             error: error.response.data.message,
             loading: false
           }
-        )
+        );
       });
   }
 
-  saveRecipe = () => {
-    const { name, description, ingredients, direction } = this.state;
+  saveRecipe() {
+    const data = new FormData();
+
+    const { imageUrl, name, description, ingredients, direction } = this.state;
+
+    data.append('name', name);
+    data.append('description', description);
+    data.append('ingredients', ingredients.replace(/,/g, ';;'));
+    data.append('direction', direction);
+    data.append('image', imageUrl);
+
     this.setState(
       { loading: true }
     );
@@ -110,10 +125,7 @@ class CreateEditRecipe extends Component {
       method: 'POST',
       url: '/api/v1/recipes',
       headers: { 'x-access-token': TOKEN },
-      data: {
-        name, description, direction,
-        ingredients: ingredients.replace(/,/g, ';;'),
-      }
+      data
     })
       .then((response) => {
         const { success } = response.data;
@@ -128,11 +140,11 @@ class CreateEditRecipe extends Component {
             error: error.response.data.message,
             loading: false
           }
-        )
+        );
       });
   }
 
-  showHeading = () => {
+  showHeading() {
     const { recipeId } = this.props;
     if (recipeId) {
       return 'Edit ';
@@ -141,7 +153,7 @@ class CreateEditRecipe extends Component {
     }
   }
 
-  renderButton = () => {
+  renderButton() {
     const { recipeId } = this.props;
     if (recipeId) {
       return (
@@ -160,7 +172,7 @@ class CreateEditRecipe extends Component {
     }
   }
 
-  renderForm = () => {
+  renderForm() {
     const { name, description, ingredients, direction, loading } = this.state;
     return (
       <Form>
@@ -170,15 +182,26 @@ class CreateEditRecipe extends Component {
           placeholder='Enter recipe name'
           value={name}
           onChange={(event) => {
-            this.storeToState('name', event.target.value)
+            this.storeToState('name', event.target.value);
           }} />
+        <Form.Field>
+          <label>Recipe Image</label>
+          <input
+            disabled={this.props.recipeId !== null}
+            type='file'
+            accept="image/*"
+            onChange={(event) => {
+              this.storeToState('imageUrl', event.target.files[FIRST_INDEX]);
+            }}
+          />
+        </Form.Field>
         <Form.Input
           disabled={loading}
           label='Recipe Description'
           placeholder='Enter a short description'
           value={description}
           onChange={(event) => {
-            this.storeToState('description', event.target.value)
+            this.storeToState('description', event.target.value);
           }} />
         <Form.Input
           disabled={loading}
@@ -186,7 +209,7 @@ class CreateEditRecipe extends Component {
           placeholder='Enter ingredient list separated by comma'
           value={ingredients.replace(/;;/g, ',')}
           onChange={(event) => {
-            this.storeToState('ingredients', event.target.value)
+            this.storeToState('ingredients', event.target.value);
           }} />
         <Form.TextArea
           disabled={loading}
@@ -194,17 +217,17 @@ class CreateEditRecipe extends Component {
           placeholder='Enter recipe preparation procedure'
           value={direction}
           onChange={(event) => {
-            this.storeToState('direction', event.target.value)
+            this.storeToState('direction', event.target.value);
           }} />
       </Form>
-    )
+    );
   }
 
   render() {
     return (
       <Modal open={this.props.modal === 'create_edit_recipe'}>
         <Modal.Header>
-          {this.showHeading()} Recipe
+          {this.showHeading()} a Recipe
           </Modal.Header>
         <Modal.Content>
           {this.renderForm()}
@@ -215,13 +238,13 @@ class CreateEditRecipe extends Component {
         <Modal.Actions>
           <Button negative icon='close' labelPosition='right' content='Cancel'
             onClick={() => {
-              this.closeModal()
+              this.closeModal();
             }} />
 
           {this.renderButton()}
         </Modal.Actions>
       </Modal>
-    )
+    );
   }
 }
 
@@ -229,7 +252,19 @@ const mapStateToProps = (state) => {
   return {
     modal: state.dialog,
     recipeId: state.recipe.id
-  }
-}
+  };
+};
 
-export default connect(mapStateToProps, { setDialogType, setReloadRecipes })(CreateEditRecipe);
+const actionCreators = {
+  setDialogType,
+  setReloadRecipes
+};
+
+CreateEditRecipe.propTypes = {
+  modal: PropTypes.string,
+  setReloadRecipes: PropTypes.func,
+  recipeId: PropTypes.number,
+  setDialogType: PropTypes.func
+};
+
+export default connect(mapStateToProps, actionCreators)(CreateEditRecipe);
