af3cc3477e4cf608cca92a9b618b8bb80657a4a4
lawal lanre <larrystonegroup@gmail.com>
2017-12-17 15:41:08 +0100

    auth

--- client/src/components/Home/AppIntro.jsx
+++ client/src/components/Home/AppIntro.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button } from 'semantic-ui-react';
-import efo from '../../../images/efo.jpg';
+import efo from './../../../images/efo.jpg';
 
 const AppIntro = () => (
   <div>
