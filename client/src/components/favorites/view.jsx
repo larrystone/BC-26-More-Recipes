f8c9ad6546c8318eab265ab797c6b7f5a07944e6
lawal lanre <larrystonegroup@gmail.com>
2018-01-13 22:57:26 +0100

    ft(recipes): user favorite recipe page with related actions - design UI for page - implement logic for fetching user favorite recipes - implement logic for removing recipe from favorite - link up with recipe detailed view page [Delivers #151972406]

---
+++ client/src/components/favorites/view.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import _ from 'lodash';
+
+import RecipeItem from '../commons/RecipeItem';
+import Loading from '../commons/Loading';
+import NothingFound from '../commons/NothingFound';
+
+const View = ({
+  isLoading, recipes, showDetails, addModal
+}) => {
+  if (isLoading) {
+    return (
+      <Loading
+        text={'my favorites'}
+      />
+    );
+  }
+  if (_.size(recipes) === 0) {
+    return (
+      <div>
+        <NothingFound />
+      </div>
+    );
+  }
+  return (
+    <div>
+      <div className="full-title wow fadeIn">
+        {'My Favorites'}
+      </div>
+      <div
+        style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}
+      >
+        {Object.keys(recipes).map(index => (
+          <RecipeItem
+            isFav
+            key={index}
+            recipe={recipes[index]}
+            actions={{
+              showDetails,
+              deleteFav: addModal
+            }}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+View.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
+  recipes: PropTypes.shape().isRequired,
+  showDetails: PropTypes.func.isRequired,
+  addModal: PropTypes.func.isRequired
+};
+
+export default View;
